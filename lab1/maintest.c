68be96522a3bb3124a6ebfabf80412f9fbb32f27
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2021-03-17 15:04:05 +0300

    Update maintest.c

--- lab1/maintest.c
+++ lab1/maintest.c
@@ -6,14 +6,14 @@ void test1(void){
     assert(count(5) == 3);
     assert(count(1) == 1);
     assert(count(0) == 0);
-    printf("Count tests passed successfully");
+    printf("Count tests passed successfully.\n");
 }
 
 void test2(void){
     assert(countAny(5, 1) == 8);
     assert(countAny(4, 2) == 2);
     assert(countAny(4, 3) == 1);
-    printf("CountAny tests passed successfully");
+    printf("CountAny tests passed successfully.\n");
 }
 
 #undef main
@@ -21,6 +21,6 @@ void test2(void){
 int main(void){
     test1();
     test2();
-    printf("All tests passed successfully");
+    printf("\nAll tests passed successfully.");
     return 0;
 }
