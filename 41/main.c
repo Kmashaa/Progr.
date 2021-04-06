b119b28eec5afd8c011524478454f775970742b2
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2021-04-06 14:03:46 +0300

    Update main.c

--- 41/main.c
+++ 41/main.c
@@ -13,28 +13,21 @@ int main() {
 	char ff,ss,tt;
 	int counter2 = 0;int k = 0;
 	tx = (char*)malloc(1000 * sizeof(char));
-
 	scanf("%999[^&]s", tx);
-	  
-	while (tx[length] >= 'a' && tx[length] <= '~') {
-		
+	while (tx[length] >= 'a' && tx[length] <= '~') {	
 		length++;
 	}
-	
-	
 	i = 2;
-	for (i; i < length+2; i++) 
-	{	ff=tx[i-2];
+	
+	for (i; i < length+2; i++) {
+		ff=tx[i-2];
 		ss=tx[i-1];
 		tt=tx[i];
-		if (check(ff,ss,tt)) 
-		{
-			if (tx[i - 1] >= 97 && tx[i - 1] <= 122) 
-			{
+		if (check(ff,ss,tt)) {
+			if (tx[i - 1] >= 97 && tx[i - 1] <= 122) {
 				tx[i - 1] -= 32;
 			}
-			else if (tx[i] >= 97 && tx[i] <= 122)
-			{
+			else if (tx[i] >= 97 && tx[i] <= 122){
 			tx[i] -= 32;
 			}
 		}
@@ -48,30 +41,23 @@ int main() {
 				break;
 			}
 			counter++;
-
 		}
 		while (tx[i + counter2+counter+1] != ' ') {
 			if (tx[i + counter2+counter+1] == '&') {
 				counter2++;
-				
 				break;
 			}
 			counter2++; 
 		}
-		if (counter = counter2)
-		{
+		if (counter = counter2){
 			for (t; t < counter; t++) {
 				if (tx[i + counter + p + 1] == tx[i + p]) {
 					p++;
 				}
 			}
 			if (p == counter && p > 0) {
-				
-
-				 newtx = (char*)malloc((length - p) * sizeof(char));
-
-				
-				for (k; k < i; k++) {
+				 newtx = (char*)malloc((length - p) * sizeof(char));				
+				 for (k; k < i; k++) {
 					newtx[k] = tx[k];
 				}
 
@@ -87,16 +73,12 @@ int main() {
 				k = 0;
 			}
 		}
-
-			counter = 0;
-			counter2 = 0;
-			p = 0;
-			t = 0;
-			
-		
+	counter = 0;
+	counter2 = 0;
+	p = 0;
+	t = 0;
 	}
 	length -= pp;
-
 
 	for (i = 0; i < length; i++) {
 		printf("%c", tx[i]);
@@ -104,12 +86,10 @@ int main() {
 	return 0;
 }
 int check(char ff,char ss, char tt){
-	if (ff == '.' && ((ss >= 97 && ss <= 122) || (tt >= 97 && tt <= 122)))
-	{
+	if (ff == '.' && ((ss >= 97 && ss <= 122) || (tt >= 97 && tt <= 122))){
 		return 1;
 	}
 	else {
 		return 0;
 	}
-	
 }
